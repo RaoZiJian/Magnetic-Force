e7ecaf9d89b969437add846330373c292dc44ef3
hunterhbb <hunterhbb@gmail.com>
2014-08-16 23:31:58 +0800

    frict sound

--- Magnetic/src/GameLayer.js
+++ Magnetic/src/GameLayer.js
@@ -258,9 +258,9 @@ var GameLayer = cc.Layer.extend({
         var parentLayer = player.obj.view.parent;
         if(!parentLayer.isEffectPlaying){
 
-            cc.audioEngine.playEffect(res.hit2_ogg,false);
+                cc.audioEngine.playEffect(res.hit2_ogg,false);
             parentLayer.isEffectPlaying = true;
-            parentLayer.scheduleOnce(parentLayer.resetEffect,0.2);
+            parentLayer.scheduleOnce(parentLayer.resetEffect, 2);
         }
         return true;
     },
