a198651403a579e239c5b3066f9fb48af641da5a
Kirito <zijian.rao@chukong-inc.com>
2014-08-17 01:19:16 +0800

    Modify value

--- Magnetic/src/GlobalValue.js
+++ Magnetic/src/GlobalValue.js
@@ -10,7 +10,7 @@ var BackGroundElastricity = 1.2;
 
 
 //item
-var ITEM_MAXSPEED = 600;
+var ITEM_MAXSPEED = 650;
 var ITEM_WEIGHT_FACTOR = 800;
 var ITEM_INIT_FRICTION = 0.25;
 var ITEM_INIT_ELASTICITY = 0.3;
@@ -18,7 +18,7 @@ var ITEM_MAX_SPEED = 500;
 
 
 //item controller
-var INITIAL_BOMB_NUMBER = 4,
+var INITIAL_BOMB_NUMBER = 2,
     MAX_BOMB_NUMBER = 10,
     BORN_INTERVAL = 10,
     PRIMI_BORN_NUMBER = 2,
@@ -31,8 +31,8 @@ var INITIAL_BOMB_NUMBER = 4,
 
 //magnet system
 var EFFECTIVE_MAGNET_DIS = 800;
-var PLAYER_INIT_MH = 450;
-var SAFE_DIS_ADD = 0.35;
+var PLAYER_INIT_MH = 400;
+var SAFE_DIS_ADD = 0.5;
 var REPULSIVE_FORCE_MUTIPLE = 2;
 
 
@@ -47,10 +47,10 @@ var PLAYER_ATTRACT_TOP_FORCE = cp.v(0, 2);
 
 //bomb
 var EXPLODE_TIME = 15+Math.random()*10;
-var EXPLODE_WARNNING_TIME = 3;//the bomb will be explode
+var EXPLODE_WARNNING_TIME = 1;//the bomb will be explode
 var EXPLODE_WEIGHT = 20;
 var EXPLODE_RADIUS = 60;
 var EXPLODE_ELASTICITY = 10;
-var BOMB_JUMP_ADD_SPEED = 200;
+var BOMB_JUMP_ADD_SPEED = 100;
 
 var GLOBAL_COL_TYPE = 0;
