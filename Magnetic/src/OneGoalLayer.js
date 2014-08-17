571c88196defe61793c4a57520c223c2e5944c66
pandamicro <lphuabin@gmail.com>
2014-08-17 13:20:01 +0800

    no message

--- Magnetic/src/OneGoalLayer.js
+++ Magnetic/src/OneGoalLayer.js
@@ -14,14 +14,25 @@ var OneGoalLayer = GameLayer.extend({
         if (result != ScoreController.HIT_NOTING) {
             this.gameController.gameOverAction();
         }
-    }
+    },
 
 //    onEnter : function () {
 //        this._super();
 //        Physics.addCollisionHandler(Player.COL_TYPE, Bomb.EXPLODE_COL_TYPE, null, this.playerBeExplode, null, null);
 //    },
 //
-//    playerBeExplode : function (arb, space, ptr) {
-//
-//    },
+    playerTouchItem : function (arb, space, ptr) {
+        var shapes = arb.getShapes();
+        var player = shapes[0].obj.view;
+        var item = shapes[1].obj.view;
+
+        // Own by first player (nasty one)
+        if (player == this.f_player) {
+            
+        }
+        // naughty one
+        else if (player == this.s_player) {
+
+        }
+    }
 });
